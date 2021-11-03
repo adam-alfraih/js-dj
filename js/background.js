e23f0820e10dbe3ba9155b76ab67e00724ba47db
John Doe <adamalfraih@gmail.com>
2021-11-03 16:57:38 +0100

    drop cd to check if its the right or wrong cd

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 class Background {
     draw() {
-      if(game.checkedCd === 'noCd' || game.checkedCd === 'undefined') {
-      image(game.backgroundImage, 0, 0, width, height);
-    } else if (game.checkedCd === 'wrongCd') {
+       if (game.checkedCd === 'wrongCd' && !game.hasCd) {
       image(game.backgroundFail, 0, 0, width, height);
-    } else if (game.checkedCd === 'rightCd') {
+    } else if (game.checkedCd === 'rightCd' && !game.hasCd) {
       image(game.backgroundSuccess, 0, 0, width, height);
+    } else {
+      image(game.backgroundImage, 0, 0, width, height);
     }
   }
 }
--- js/objects.js
+++ js/objects.js
@@ -43,30 +43,28 @@ class ChatBubble {
 
 
     draw() {
-        console.log(game.checkedCd)
+ 
         // if there is no cd then display this image
-        if (game.checkedCd === 'noCd') {
-          //  console.log(game.checkedCd)
-            image(this.chatBubble, 835, 290, 150, 150)
-
-        } else if (game.checkedCd === 'wrongCd') {
+        if (game.checkedCd === 'wrongCd' && !game.hasCd) {
             //console.log(game.checkedCd)
             image(this.chatBubbleFail, 835, 290, 150, 150)
-            
 
-        } 
-        else if (game.checkedCd === 'rightCd') {
+
+        }
+        else if (game.checkedCd === 'rightCd' && !game.hasCd) {
             //console.log(game.checkedCd)
             image(this.chatBubbleSuccess, 835, 290, 150, 150)
 
 
-        } 
+        }
         else if (game.checkedCd === 'undefined') {
             //console.log(game.checkedCd)
-            
+
             image(this.chatBubble2, 835, 290, 150, 150)
 
-        } 
+        } else {
+            image(this.chatBubble, 835, 290, 150, 150)
+        }
         // if checkedCd = 'true'
         //   image(this.chatBubble, 835, 290, 150, 150)
 
